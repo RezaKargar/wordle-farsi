2e8f554c5e7ce85194aaabf8e7e6925ae4e64e5e
Pedram Yousefi <pedram.yousefi23@gmail.com>
2025-03-15 14:40:53 +0330

    Adds around 750 new words to the game

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -14,12 +14,12 @@ export const isWinningWord = (word: string) => {
 
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
-  const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
+  // const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
+  const epochMs = new Date('July 10, 2024 00:00:00').valueOf()
   const now = Date.now()
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
   const nextday = (index + 1) * msInDay + epochMs
-
   return {
     solution: WORDS[index % WORDS.length].toUpperCase(),
     solutionIndex: index,
